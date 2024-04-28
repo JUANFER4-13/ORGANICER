7e132af89f01b515ed94f70c6056c14b943783d4
JUANFER4-13 <158770161+JUANFER4-13@users.noreply.github.com>
2024-04-27 19:11:31 -0500

    Add files via upload

---
+++ jsLOG.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+    $("#do_login").click(function() { 
+       closeLoginInfo();
+       $(this).parent().find('span').css("display","none");
+       $(this).parent().find('span').removeClass("i-save");
+       $(this).parent().find('span').removeClass("i-warning");
+       $(this).parent().find('span').removeClass("i-close");
+       
+        var proceed = true;
+        $("#login_form input").each(function(){
+            
+            if(!$.trim($(this).val())){
+                $(this).parent().find('span').addClass("i-warning");
+            	$(this).parent().find('span').css("display","block");  
+                proceed = false;
+            }
+        });
+       
+        if(proceed) //¡Todo se ve bien! proceder...
+        {
+            $(this).parent().find('span').addClass("i-save");
+            $(this).parent().find('span').css("display","block");
+        }
+    });
+    
+    //restablecer los resultados anteriores y ocultar todos los mensajes en .keyup()
+    $("#login_form input").keyup(function() { 
+        $(this).parent().find('span').css("display","none");
+    });
+ 
+  openLoginInfo();
+  setTimeout(closeLoginInfo, 1000);
+});
+
+function openLoginInfo() {
+    $(document).ready(function(){ 
+    	$('.b-form').css("opacity","0.01");
+      $('.box-form').css("left","-37%");
+      $('.box-info').css("right","-37%");
+    });
+}
+
+function closeLoginInfo() {
+  $('.b-form').css('opacity', '1');
+  $('.box-form, .box-info').css('left', '0px');
+}
+
+
+$(window).on('resize', function(){
+      closeLoginInfo();
+});
